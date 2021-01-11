65dcd909112f21420627c7119d50239f767fecbc
pipe2442 <felipeangulo2442@gmail.com>
2021-01-11 16:41:13 -0500

    Started  game logic dev

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,19 @@
-puts 'we will create the board here'
+
+def printBoard(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+    
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
